5a0ad43253e80acb8fae8d0d5011b1dce55e596b
suku <sukuwc@riseup.net>
2024-01-02 11:18:58 +0100

    SUKU test2

--- .github/workflows/freecad_export2.py
+++ .github/workflows/freecad_export2.py
@@ -11,9 +11,32 @@ else:
 
 App.openDocument("Mechanical/Design/PCBA-ENDLESSPOT/PCBA-ENDLESSPOT.FCStd")
 
-objs = App.ActiveDocument.Objects
 for obj in objs:
   sono=App.ActiveDocument.getObject(obj.Name)
+  sono.ViewObject.show()
+
+  # Ez kell ide
+  FreeCADGui.updateGui()
+  FreeCADGui.updateGui()
+  FreeCADGui.updateGui()
+  FreeCADGui.updateGui()
+  FreeCADGui.updateGui()
+  FreeCADGui.updateGui()
+
+  if sono.TypeId == "PartDesign::Body":
+
+    if "png" in export_list:
+      print(obj.Label, obj.Name, "PNG")
+      exportScreenshot(obj.Label, "temp/"+obj.Label+".png")
+
+    if "step" in export_list:
+      print(obj.Label, obj.Name, "STEP")
+
+      sono.Shape.exportStep("temp/"+obj.Label+".step")
+
+    if "stl" in export_list:
+      print(obj.Label, obj.Name, "STL")
+      sono.Shape.exportStl("temp/"+obj.Label+".stl")
 
   if sono.TypeId == "App::Part":
     print(obj.Label, obj.Name, "STEP")
@@ -23,3 +46,13 @@ for obj in objs:
     print(__objs__)
     import ImportGui
     ImportGui.export(__objs__,"temp/"+obj.Label+"2.step")
+
+  elif sono.TypeId == "TechDraw::DrawPage":
+    if "pdf" in export_list:
+      print(obj.Label, obj.Name, "DRAW")
+      TechDrawGui.export([sono],u"temp/"+obj.Label+".pdf")
+
+  sono.ViewObject.hide()
+
+
+hideAll()
